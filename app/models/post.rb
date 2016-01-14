edf9ce66333a107a850461eb1e1248f508c5457a
International <international.resource.help@gmail.com>
2016-01-13 19:32:11 -0500

    Latest version of regex. Added bad word filter

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,21 @@ class Post < ActiveRecord::Base
   validates :content, presence: true
   validates :name,    presence: true
 
+  validate :flurb_not_in_content
+  validate :names_are_proper
+
+  def flurb_not_in_content
+    %i(title content).each do |attribute|
+      if send(attribute) =~ /fl([aeiou]|\|_\|)rb/i
+        errors.add(attribute, "can't include the word \"flurb\" or a variation thereof")
+      end
+    end
+  end
+
+  def names_are_proper
+    if name != /()/
+      errors.add(name, "isn't a valid name (only letters, spaces, periods, dashes, and commas allowed)")
+    end
+  end
+
 end
